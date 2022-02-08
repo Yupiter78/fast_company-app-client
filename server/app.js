7dab0ea21fbbce0ec87e17edaa61b2639a86b1cd
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-08 04:19:49 +0300

    jfd_fast-company-app-client__server_and_work_environment_setup_3

--- server/app.js
+++ server/app.js
@@ -2,3 +2,16 @@ const express = require("express");
 const config = require("config");
 const chalk = require("chalk");
 const mongoose = require("mongoose");
+
+const app = express();
+const PORT = config.get("port") ?? 8080;
+
+// if (process.env.NODE_ENV === "production") {
+//     console.log(chalk.bgCyanBright("Production"));
+// } else {
+//     console.log(chalk.bgCyanBright("Development"));
+// }
+
+app.listen(PORT, () =>
+    console.log(chalk.bgGreen(`Server has been started on port ${PORT}...`))
+);
