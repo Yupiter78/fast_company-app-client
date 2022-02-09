58a34954cfc10ba227d802c3f6cbb5dbe38b2133
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-09 23:54:24 +0300

    jfd_fast-company-app-client__write_logic_for_registration_2_12

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const { generateUserData } = require("../utils/helpers");
+const tokenService = require("../services/token.service");
 const router = express.Router({ mergeParams: true });
 
 // /api/auth/signUp
@@ -26,11 +27,14 @@ router.post("/signUp", async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
-        const newUser = User.create({
+        const newUser = await User.create({
             ...generateUserData(),
             ...res.body,
             password: hashedPassword
         });
+
+        const tokens = tokenService.generate({ _id: newUser._id });
+        res.status(201).send({ ...tokens, userId: newUser._id });
     } catch (error) {
         res.status(500).json({
             message: "An error has occurred on the server. Try later."
