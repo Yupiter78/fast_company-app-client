38beb51b14a2f44c2ca93bfc70ab17bc67466e92
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-09 17:44:09 +0300

    jfd_fast-company-app-client__we_get_professions_and_qualities_10

--- server/routes/profession.routes.js
+++ server/routes/profession.routes.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const Profession = require("../models/Profession");
 const router = express.Router({ mergeParams: true });
+
+// async (req, res) => {}
+router.get("/", async (req, res) => {
+    try {
+        // const professions = await Profession.find();
+        // res.status(200).json({ list: professions });
+        const list = await Profession.find();
+        res.status(200).send(list);
+    } catch (error) {
+        res.status(500).json({
+            message: "An error has occurred on the server. Try later."
+        });
+    }
+});
 
 module.exports = router;
--- server/routes/quality.routes.js
+++ server/routes/quality.routes.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const Quality = require("../models/Quality");
 const router = express.Router({ mergeParams: true });
+
+router.get("/", async (req, res) => {
+    try {
+        const list = await Quality.find();
+        res.status(200).send(list);
+    } catch (error) {
+        res.status(500).json({
+            message: "An error has occurred on the server. Try later."
+        });
+    }
+});
 
 module.exports = router;
