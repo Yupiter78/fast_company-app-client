406abd6c4830744be13b047700135c12533fc50d
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-10 23:50:29 +0300

    jfd_fast-company-app-client__write_logic_to_work_with_users_17

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,38 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router({ mergeParams: true });
+
+router.patch("/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        // todo: userId === current user id
+        if (userId) {
+            const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+                new: true
+            });
+            res.send(updatedUser);
+        } else {
+            res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "An error has occurred on the server.Try later."
+        });
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const list = await User.find();
+        return res.send(list);
+    } catch (error) {
+        res.status(500).json({
+            message: "An error has occurred on the server. Try later"
+        });
+    }
+});
 
 module.exports = router;
