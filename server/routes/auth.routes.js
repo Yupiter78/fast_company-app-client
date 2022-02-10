206c39d153e534e20f195e78c6bfd63c2f8c8c22
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-10 05:04:32 +0300

    jfd_fast-company-app-client__field_validation_with_express-validator_14

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const { generateUserData } = require("../utils/helpers");
 const tokenService = require("../services/token.service");
+const { check, validationResult } = require("express-validator");
 const router = express.Router({ mergeParams: true });
 
 // /api/auth/signUp
@@ -11,38 +12,54 @@ const router = express.Router({ mergeParams: true });
 // 3. hash password
 // 4. user create
 // 5. generate tokens
-router.post("/signUp", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const exitingUser = await User.findOne({ email });
+router.post("/signUp", [
+    check("email", "Incorrect email").isEmail(),
+    check("password", "Minimum password length 8 characters").isLength({
+        min: 8
+    }),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    error: {
+                        message: "INVALID_DATA",
+                        code: 400,
+                        errors: errors.array()
+                    }
+                });
+            }
+            const { email, password } = req.body;
+            const exitingUser = await User.findOne({ email });
 
-        if (exitingUser) {
-            return res.status(400).json({
-                error: {
-                    message: "EMAIL_EXISTS",
-                    code: 400
-                }
+            if (exitingUser) {
+                return res.status(400).json({
+                    error: {
+                        message: "EMAIL_EXISTS",
+                        code: 400
+                    }
+                });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12);
+
+            const newUser = await User.create({
+                ...generateUserData(),
+                ...req.body,
+                password: hashedPassword
+            });
+
+            const tokens = tokenService.generate({ _id: newUser._id });
+            await tokenService.save(newUser._id, tokens.refreshToken);
+
+            res.status(201).send({ ...tokens, userId: newUser._id });
+        } catch (error) {
+            res.status(500).json({
+                message: "An error has occurred on the server. Try later."
             });
         }
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        const newUser = await User.create({
-            ...generateUserData(),
-            ...req.body,
-            password: hashedPassword
-        });
-
-        const tokens = tokenService.generate({ _id: newUser._id });
-        await tokenService.save(newUser._id, tokens.refreshToken);
-
-        res.status(201).send({ ...tokens, userId: newUser._id });
-    } catch (error) {
-        res.status(500).json({
-            message: "An error has occurred on the server. Try later."
-        });
     }
-});
+]);
 
 router.post("/signInWithPassword", async (req, res) => {});
 
