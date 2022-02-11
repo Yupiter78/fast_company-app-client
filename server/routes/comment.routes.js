cb5dce7754ef1d3e5f6afadd665e4a4f2cb03b79
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-11 03:38:33 +0300

    jfd_fast-company-app-client__creating_comment_functionality_20

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,4 +1,55 @@
 const express = require("express");
+const auth = require("../middleware/auth.middleware");
+const Comment = require("../models/Comment");
 const router = express.Router({ mergeParams: true });
+
+//api/comment
+router
+    .route("/")
+    .get(auth, async (req, res) => {
+        try {
+            const { orderBy, equalTo } = req.query;
+
+            const list = await Comment.find({ [orderBy]: equalTo });
+            res.send(list);
+        } catch (error) {
+            res.status(500).json({
+                message: "An error has occurred on the server. Try later."
+            });
+        }
+    })
+    .post(auth, async (req, res) => {
+        try {
+            const newComment = await Comment.create({
+                ...req.body,
+                userId: req.user._id
+            });
+            res.status(201).send(newComment);
+        } catch (error) {
+            res.status(500).json({
+                message: "An error has occurred on the server. Try later."
+            });
+        }
+    });
+
+router.delete("/:commentId", auth, async (req, res) => {
+    try {
+        const { commentId } = req.params;
+
+        // const removedComment = await Comment.find({_id: commentId});
+        const removedComment = await Comment.findById(commentId);
+
+        if (removedComment.userId.toString() === req.user._id) {
+            await removedComment.remove();
+            res.send(null);
+        } else {
+            res.status(401).json({ message: "Unauthorized", code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "An error has occurred on the server. Try later."
+        });
+    }
+});
 
 module.exports = router;
