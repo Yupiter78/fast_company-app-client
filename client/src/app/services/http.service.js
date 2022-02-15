4224da7bb859ac4d87dd29e3e0195297db867f87
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-15 16:51:07 +0300

    refactor: change server integration

--- client/src/app/services/http.service.js
+++ client/src/app/services/http.service.js
@@ -11,13 +11,15 @@ const http = axios.create({
 
 http.interceptors.request.use(
     async function (config) {
+        const expiresDate = localStorageService.getTokenExpiresDate();
+        const refreshToken = localStorageService.getRefreshToken();
+        const isExpired = refreshToken && expiresDate < Date.now();
+
         if (configFile.isFireBase) {
             const containSlash = /\/$/gi.test(config.url);
             config.url =
                 (containSlash ? config.url.slice(0, -1) : config.url) + ".json";
-            const expiresDate = localStorageService.getTokenExpiresDate();
-            const refreshToken = localStorageService.getRefreshToken();
-            if (refreshToken && expiresDate < Date.now()) {
+            if (isExpired) {
                 const data = await authService.refresh();
 
                 localStorageService.setTokens({
@@ -31,6 +33,25 @@ http.interceptors.request.use(
             if (accessToken) {
                 config.params = { ...config.params, auth: accessToken };
             }
+        } else {
+            if (isExpired) {
+                const data = await authService.refresh();
+
+                // localStorageService.setTokens({
+                //     refreshToken: data.refreshToken,
+                //     accessToken: data.accessToken,
+                //     expiresIn: data.expiresIn,
+                //     userId: data.userId
+                // });
+                localStorageService.setTokens(data);
+            }
+            const accessToken = localStorageService.getAccessToken();
+            if (accessToken) {
+                config.headers = {
+                    ...config.headers,
+                    Authorization: `Bearer ${accessToken}`
+                };
+            }
         }
         return config;
     },
@@ -38,6 +59,7 @@ http.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+
 function transormData(data) {
     return data && !data._id
         ? Object.keys(data).map((key) => ({
@@ -45,6 +67,7 @@ function transormData(data) {
           }))
         : data;
 }
+
 http.interceptors.response.use(
     (res) => {
         if (configFile.isFireBase) {
