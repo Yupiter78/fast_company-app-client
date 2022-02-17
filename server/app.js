d60aefa28a35a84339953e721c412aef797ea72c
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-17 03:13:26 +0300

    feat: implement Production mode for the server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const chalk = require("chalk");
+const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const initDatabase = require("./startUp/initDatabase");
@@ -16,11 +17,19 @@ app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
-// if (process.env.NODE_ENV === "production") {
-//     console.log(chalk.bgCyanBright("Production"));
-// } else {
-//     console.log(chalk.bgCyanBright("Development"));
-// }
+if (process.env.NODE_ENV === "production") {
+    console.log(chalk.bgCyanBright("Production"));
+} else {
+    console.log(chalk.bgCyanBright("Development"));
+}
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "client")));
+    const indexPath = path.join(__dirname, "client", "index.html");
+    app.get("*", (req, res) => {
+        res.sendFile(indexPath);
+    });
+}
 
 async function start() {
     try {
